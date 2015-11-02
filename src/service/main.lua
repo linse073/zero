2c3e6e8aaf03fe9a5fac5a4e952b052defab267d
linse073 <linse073@gmail.com>
2015-11-02 15:25:04 +0800

    call skynet.exit() in main.lua

--- src/service/main.lua
+++ src/service/main.lua
@@ -1,12 +1,14 @@
 local skynet = require "skynet"
 
 skynet.start(function()
-	local loginserver = skynet.newservice("logind")
-	local gate = skynet.newservice("gated", loginserver)
+	local loginserver = skynet.newservice("login")
+	local gate = skynet.newservice("gate", loginserver)
 
 	skynet.call(gate, "lua", "open" , {
 		port = 8888,
 		maxclient = 64,
 		servername = "sample",
 	})
+    
+    skynet.exit()
 end)

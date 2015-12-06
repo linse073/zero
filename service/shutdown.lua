dd25e6150ded53dbac394c8818d914fee1eebfc9
linse073 <linse073@gmail.com>
2015-12-06 12:19:57 +0800

    add shutdown

--- service/shutdown.lua
+++ service/shutdown.lua
@@ -3,4 +3,6 @@ local skynet = require "skynet"
 skynet.start(function()
     local server_mgr = skynet.queryservice("server_mgr")
     skynet.call(server_mgr, "lua", "shutdown")
+    skynet.error("shutdown finish.")
+    skynet.exit()
 end)

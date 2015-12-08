6517477990ed4b451f947df971bb1ae8e7aa99ee
linse073 <linse073@gmail.com>
2015-12-08 21:10:56 +0800

    log

--- service/userlog.lua
+++ service/userlog.lua
@@ -1,18 +1,51 @@
 local skynet = require "skynet"
 require "skynet.manager"
+local timer = require "timer"
 
 local date = os.date
 local floor = math.floor
+
+local p
+local f
 
 skynet.register_protocol {
 	name = "text",
 	id = skynet.PTYPE_TEXT,
 	unpack = skynet.tostring,
 	dispatch = function(_, address, msg)
-		print(string.format("[%s][%s]: %s", skynet.address(address), date("%m/%d/%Y %X", floor(skynet.time())), msg))
+		p(string.format("[%s][%s]: %s", skynet.address(address), date("%X", floor(skynet.time())), msg))
 	end
 }
 
+local function change_log()
+    if f then
+        f:close()
+    end
+    local name = date("%m_%d_%Y.log", floor(skynet.time()))
+    f = io.open(name, "a")
+end
+
+local function print_file(msg)
+    
+end
+
+local CMD = {}
+
+function CMD.day_routine(key)
+    timer.call_day_routine(key)
+end
+
 skynet.start(function()
 	skynet.register ".logger"
+    if skynet.getenv("daemon") then
+        change_log()
+        timer.add_day_routine("change_log", change_log)
+        p = print_file
+    else
+        p = print
+    end
+
+	skynet.dispatch("lua", function(session, source, command, ...)
+		assert(CMD[command])(...)
+	end)
 end)

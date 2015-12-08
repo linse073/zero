dee3432149d477430b17770b87d974f3c3f1652d
linse073 <linse073@gmail.com>
2015-12-08 23:07:53 +0800

    log

--- service/userlog.lua
+++ service/userlog.lua
@@ -1,6 +1,5 @@
 local skynet = require "skynet"
 require "skynet.manager"
-local timer = require "timer"
 
 local date = os.date
 local floor = math.floor
@@ -13,7 +12,7 @@ skynet.register_protocol {
 	id = skynet.PTYPE_TEXT,
 	unpack = skynet.tostring,
 	dispatch = function(_, address, msg)
-		p(string.format("[%s][%s]: %s", skynet.address(address), date("%X", floor(skynet.time())), msg))
+		p(string.format("[%s][%s]: %s", skynet.address(address), date("%m/%d/%Y %X", floor(skynet.time())), msg))
 	end
 }
 
@@ -21,25 +20,21 @@ local function change_log()
     if f then
         f:close()
     end
-    local name = date("%m_%d_%Y.log", floor(skynet.time()))
-    f = io.open(name, "a")
+    local name = skynet.getenv(root).."log/"..date("%m_%d_%Y.log", floor(skynet.time()))
+    f = assert(io.open(name, "a"), string.format("Can't open log file %s.", name))
+    skynet.timeout(8640000, change_log) -- one day
 end
 
 local function print_file(msg)
-    
-end
-
-local CMD = {}
-
-function CMD.day_routine(key)
-    timer.call_day_routine(key)
+    f:write(msg)
+    f:flush()
 end
 
 skynet.start(function()
 	skynet.register ".logger"
     if skynet.getenv("daemon") then
         change_log()
-        timer.add_day_routine("change_log", change_log)
+        skynet.timeout(8640000, change_log) -- one day
         p = print_file
     else
         p = print

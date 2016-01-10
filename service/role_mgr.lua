58487669070db07d0c05f57bbbd9cdb59e49e6f3
linse073 <linse073@gmail.com>
2016-01-10 16:39:12 +0800

    correct

--- service/role_mgr.lua
+++ service/role_mgr.lua
@@ -109,7 +109,7 @@ skynet.start(function()
 	skynet.dispatch("lua", function(session, source, command, ...)
 		local f = assert(CMD[command])
         if session == 0 then
-            f(..)
+            f(...)
         else
             skynet.retpack(f(...))
         end

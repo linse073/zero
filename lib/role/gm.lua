8f358b183fbecbf3e71decca9707dfb98517a5ef
linse073 <linse073@gmail.com>
2016-01-18 17:48:09 +0800

    gm add exp

--- lib/role/gm.lua
+++ lib/role/gm.lua
@@ -3,10 +3,13 @@ local share = require "share"
 local util = require "util"
 
 local item
+local role
 
 local update_user = util.update_user
 local error_code
 local itemdata
+local expdata
+local base
 local data
 
 local gm = {}
@@ -15,10 +18,13 @@ local proc = {}
 skynet.init(function()
     error_code = share.error_code
     itemdata = share.itemdata
+    expdata = share.expdata
+    base = share.base
 end)
 
 function gm.init_module()
     item = require "role.item"
+    role = require "role.role"
     return proc
 end
 
@@ -42,6 +48,31 @@ function proc.add_item(msg)
     end
     local p = update_user()
     item.add_by_itemid(p, msg.num, d)
+    return "update_user", {update=p}
+end
+
+function proc.add_exp(msg)
+    if data.user.gm_level == 0 then
+        error{code = error_code.ROLE_NO_PERMIT}
+    end
+    local p = update_user()
+    role.add_exp(p, msg.exp)
+    return "update_user", {update=p}
+end
+
+function proc.add_level(msg)
+    local user = data.user
+    if user.gm_level == 0 then
+        error{code = error_code.ROLE_NO_PERMIT}
+    end
+    local level = user.level + msg.level
+    if level > base.MAX_LEVEL then
+        level = base.MAX_LEVEL
+    end
+    local ed = assert(expdata[level-1], string.format("No exp data %d.", level-1))
+    local exp = ed.HeroExp - user.exp
+    local p = update_user()
+    role.add_exp(p, exp)
     return "update_user", {update=p}
 end
 

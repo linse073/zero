4249486f182f36fbb43353f2d48c00481497ce4f
linse073 <linse073@gmail.com>
2016-04-20 20:25:32 +0800

    update random

--- lib/random.lua
+++ lib/random.lua
@@ -12,10 +12,7 @@ function random.rand(min, max)
     if not max then
         min, max = 1, min
     end
-    local num = rand.rand(r, min, max)
-    print("rand num", num)
-    return num
-    -- return rand.rand(r, max)
+    return rand.rand(r, min, max)
 end
 
 return random

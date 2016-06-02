665e67679d0fb5c6610137223b96d2686533b6d8
linse073 <linse073@gmail.com>
2016-06-02 16:31:27 +0800

    correct

--- lib/role/role.lua
+++ lib/role/role.lua
@@ -499,6 +499,7 @@ local function enter_game(msg)
             ret[key] = pack
         end
     end
+    ret.stage_award = {1}
     -- local key, pack = stage.pack_award()
     -- ret[key] = pack
     timer.add_routine("save_role", role.save_routine, 300)

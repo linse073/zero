d084633814cad6bcb3d86ba208cb74efb5c8ef32
linse073 <linse073@gmail.com>
2016-07-13 15:49:55 +0800

    correct

--- service/main.lua
+++ service/main.lua
@@ -46,8 +46,8 @@ skynet.start(function()
     skynet.uniqueservice("explore_mgr")
     -- TODO: server shutdown time
     skynet.call(save_explore, "lua", "open")
-    skynet.uniqueservice("agent_mgr")
     skynet.uniqueservice("mail_mgr")
+    skynet.uniqueservice("agent_mgr")
 
 	local loginserver = skynet.newservice("logind")
     local gate = skynet.newservice("gated", loginserver)
